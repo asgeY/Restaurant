abe1f131908507d32f5de5af23a5b60f009bd09f
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-25 17:13:09 -0500

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Matvey on 4/25/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
