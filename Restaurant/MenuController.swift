5ce8c8e53d84b886a1f3d7caec360407f91755e0
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-25 22:40:01 -0500

    add functionality for parsing responses

--- Restaurant/MenuController.swift
+++ Restaurant/MenuController.swift
@@ -19,6 +19,13 @@ class MenuController {
         let task = URLSession.shared.dataTask(with: categoryURL) {
             (data, response, error) in
             
+            if let data = data,
+                let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+                let categories = jsonDictionary["categories"] as? [String] {
+                completion(categories)
+            } else {
+                completion(nil)
+            }
         }
         
         task.resume()
@@ -35,6 +42,13 @@ class MenuController {
         
         let task = URLSession.shared.dataTask(with: menuURL) {
             (data, response, error) in
+            
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
+                completion(menuItems.items)
+            } else {
+                completion(nil)
+            }
             
         }
         
@@ -58,6 +72,13 @@ class MenuController {
         request.httpBody = jsonData
         let task = URLSession.shared.dataTask(with: request) {
             (data, response, error) in
+            
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
+                completion(preparationTime.prepTime)
+            } else {
+                completion(nil)
+            }
             
         }
         
