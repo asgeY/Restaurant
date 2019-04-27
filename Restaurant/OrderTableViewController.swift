285c7cb374aceb4f3689be67c72b63d58ea59549
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-27 11:28:16 -0500

    add order submission and confirmation functionality

--- Restaurant/OrderTableViewController.swift
+++ Restaurant/OrderTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class OrderTableViewController: UITableViewController {
     
+    var orderMinutes = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = editButtonItem
@@ -21,6 +23,57 @@ class OrderTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    @IBAction func submitTapped(_ sender: UIBarButtonItem) {
+        
+        let orderTotal = MenuController.shared.order.menuItems.reduce(0.0) {
+            (result, menuItem) -> Double in return result + menuItem.price
+        }
+        
+        let formattedOrder = String(format: "$%.2f", orderTotal)
+        
+        let alert = UIAlertController(title: "Confirm Order", message: "You are about to submit your order with a total of \(formattedOrder)", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Submit", style: .default) { action in self.uploadOrder()
+        })
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func uploadOrder() {
+        
+        let menuIds = MenuController.shared.order.menuItems.map { $0.id }
+        
+        MenuController.shared.submitOrder(forMenuIDs: menuIds) {
+            (minutes) in
+            DispatchQueue.main.async {
+                if let minutes = minutes {
+                    self.orderMinutes = minutes
+                    self.performSegue(withIdentifier: "ConfirmationSegue", sender: nil)
+                }
+            }
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "ConfirmationSegue" {
+            
+            let orderConfirmationViewController = segue.destination as! OrderConfirmationViewController
+            
+            orderConfirmationViewController.minutes = orderMinutes
+            
+        }
+        
+    }
+    
+    @IBAction func unwindToOrderList(segue: UIStoryboardSegue) {
+        
     }
 
     // MARK: - Table view data source
